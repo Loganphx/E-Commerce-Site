f454cf626950e2cffaef8a685878305c5c166046
Logan Ingram <loganphx1@gmail.com>
2024-09-10 16:44:13 -0500

    - Updated Default Schema to e-commerce

--- API/Infrastructure/Data/StoreContext.cs
+++ API/Infrastructure/Data/StoreContext.cs
@@ -1,12 +1,23 @@
 using Core.Entities;
+using Infrastructure.Config;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Data;
 
 public class StoreContext : DbContext
 {
+    public DbSet<Product> Products { get; set; }
+    
     public StoreContext(DbContextOptions options) : base(options)
     {
-        public DbSet<Product> Products { get; set; }
     }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.HasDefaultSchema("e-commerce");
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ProductConfiguration).Assembly);
+    }
+    
 }
