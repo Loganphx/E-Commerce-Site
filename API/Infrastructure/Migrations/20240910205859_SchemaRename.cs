d475ddacaa0c61cc21bdfa55fb95edf7564a5018
Logan Ingram <loganphx1@gmail.com>
2024-09-10 16:51:49 -0500

    - Started Tracking Migrations

---
+++ API/Infrastructure/Migrations/20240910205859_SchemaRename.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class SchemaRename : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "e-commerce");
+
+            migrationBuilder.RenameTable(
+                name: "Products",
+                newName: "Products",
+                newSchema: "e-commerce");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "Products",
+                schema: "e-commerce",
+                newName: "Products");
+        }
+    }
+}
