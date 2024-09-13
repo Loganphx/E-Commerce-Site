3033e31f59d77d21713ed7b7aa16a7943f353efa
Logan Ingram <loganphx1@gmail.com>
2024-09-12 23:44:22 -0500

    - Created BuggyController to test all types for errors

---
+++ API/API/Controllers/BuggyController.cs
@@ -0,0 +1,38 @@
+using System;
+using Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class BuggyController : BaseApiController
+{
+    [HttpGet("unauthorized")]
+    public IActionResult GetUnauthorized()
+    {
+        return Unauthorized();
+    } 
+    
+    [HttpGet("badrequest")]
+    public IActionResult GetBadRequest()
+    {
+        return BadRequest("Not a good request");
+    }
+    
+    [HttpGet("notfound")]
+    public IActionResult GetNotFound()
+    {
+        return NotFound();
+    }
+    
+    [HttpGet("internalerror")]
+    public IActionResult GetInternalError()
+    {
+        throw new Exception("Test exception");
+    }
+    
+    [HttpGet("validationerror")]
+    public IActionResult GetValidationError(Product product)
+    {
+        return Ok();
+    }
+}
