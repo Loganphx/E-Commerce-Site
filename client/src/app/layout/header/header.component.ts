fa38c6c45b9b06d9d1b45631c3a2fa92b7f1eb23
Logan Ingram <loganphx1@gmail.com>
2024-09-23 16:55:18 -0500

    - Created the Header Component

---
+++ client/src/app/layout/header/header.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import {MatIcon} from "@angular/material/icon";
+import {MatButton} from "@angular/material/button";
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [
+    MatIcon,
+    MatButton
+  ],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+
+}
